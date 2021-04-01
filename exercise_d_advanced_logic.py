ee911618dfd92b6d2e04230dbd8035b92487083c
Spencer <Spencer.Taber@yahoo.com>
2021-04-01 22:52:27 +0100

    completed tasks 1 & 2 of exercise d

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -4,11 +4,46 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
 
+even_integ = []
+
+for even_number in numbers:
+    if even_number % 2 == 0:
+        even_integ.append(even_number)
+
+print(even_integ)
+
+
 
 # 2. Print the difference between the largest and smallest value:
 
+# create function to invoke the list, numbers
+# use max and min key words to work define empty variable
+
+def b_difference(list):
+     difference= max(list) - min(list)
+     return difference
+
+    
+
+
+
+print(b_difference(numbers))
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
+
+
+#this code only checks for duplicates doesnt fulfil task
+
+# def check_duplicates(numbers):
+
+#     for duplicates in numbers:
+#         if numbers.count(duplicates) > 1:
+#             return True
+        
+#         return False
+
+# print(check_duplicates(numbers))
+
 
 
 # 4. Print the sum of the numbers, 
